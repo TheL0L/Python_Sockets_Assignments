b203135851c8295ba94c91e0b0e0700e30175b6e
Sergei <sergeiyakima1@gmail.com>
2024-06-12 15:51:04 +0300

    mostly implemented client-server architecture for assignment 02

--- 02/cs.py
+++ 02/cs.py
@@ -1,20 +1,84 @@
-import socket
-import threading
+import socket, threading
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-sock.bind(('0.0.0.0', 9999))
-sock.listen(1)
+def port_select(PORTS : list) -> int:
+    print('Select a port:')
+    for p in range(len(PORTS)):
+        print(f'{p}. {PORTS[p]}')
+    x = int(input())
+    return x if 0 <= x < len(PORTS) else -1
 
-def respond_to_client(conn_socket, client_address):
-    print('start listening from', client_address)
+def setup_listener(port : int) -> socket:
+    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listener.bind(('0.0.0.0', port))
+    listener.listen(1)
+    return listener
+
+def await_connections(listener : socket) -> None:
+    LOG('awaiting for connections...')
+    while True:  # TODO: think of a way to stop the loop
+        conn, client_address = listener.accept()
+        LOG(f'connection with {client_address} established.')
+        threading.Thread(target=respond_to_client, args=(conn, client_address)).start()
+
+def attempt_handshake(ip : str, port : int) -> socket:
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        sock.connect((ip, port))
+        return sock
+    except OSError as err:
+        #LOG(f'connection to {ip}:{port} failed. {err}')  # log failed connections
+        return None
+
+def respond_to_client(conn_socket : socket, client_address : tuple[str, int]) -> None:
     while True:
-        data = conn_socket.recv(1024)
-        print('recieved from', client_address, 'text', data.decode())
-        conn_socket.send(b'Echo: ' + data)
+        data = conn_socket.recv(_PACKET_SIZE)
+        message = data.decode()
+        print(f'{client_address}: {message}')
+        if message.lower() == 'hello':
+            conn_socket.send(b'World')
 
-while True:
-    conn, client_address = sock.accept()
-    print('new connection from', client_address)
+def LOG(message : str) -> None:
+    print(f'LOG: {message}')
 
-    threading.Thread(target=respond_to_client, args=(conn, client_address)).start()
+
+# constants
+_LOCALHOST = '127.0.0.1'
+_PACKET_SIZE = 1024
+
+# (hopefully) constant list of ports
+_PORTS = [30000, 30001, 30002, 30003, 30004]
+
+# port selection
+port_index = port_select(_PORTS)
+while port_index == -1:
+    LOG('ERROR: Selected port is invalid or taken.\n')
+    port_index = port_select(_PORTS)
+
+# listener setup
+listener = setup_listener(_PORTS[port_index])
+listener_thread = threading.Thread(target=await_connections, args=(listener,))
+listener_thread.start()
+
+# handshake attempts with everyone, except ourselves
+connections = [attempt_handshake(_LOCALHOST, p) for p in _PORTS if p != _PORTS[port_index]]
+
+# discard any failed connections
+connections = [c for c in connections if c is not None]
+
+# send messages over successful connections
+for c in connections:
+    c.send(b'Hello')
+    # message is not being received by anyone, should investigate the listener 
+
+# merge listener with main thread, and await end of program execution
+listener_thread.join()
+
+# close listener and all connections
+LOG('closing all connections...')
+listener.close()
+for c in connections:
+    c.close()
+
+LOG('done.')
 
