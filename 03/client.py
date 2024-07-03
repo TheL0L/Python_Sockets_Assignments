4af97fcb5c65a1e3422ad7fd06d9fb4bfcec3c8a
Sergei <sergeiyakima1@gmail.com>
2024-07-03 19:11:32 +0300

    rewritten client code

--- 03/client.py
+++ 03/client.py
@@ -1,26 +1,57 @@
-from threading import Thread
-import socket
-import sys
+import socket, struct, sys, threading
+import shared
 
-server_addr = ('127.0.0.1', 9999)
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-my_name = input('Enter your name: ')
-sock.sendto(my_name.encode(), server_addr)
-
-def output_recvfrom(sock):
+def await_messages(server_sock: socket.socket):
     while True:
-        data, _ = sock.recvfrom(1024)
-        if not data:
-            break
-        print(data.decode())
+        # receive header bytes
+        try:
+            bytes_header = server_sock.recv(shared._HEADER_SIZE)
+            _type, _sub_type, _len, _sub_len = struct.unpack(bytes_header)
+        except OSError as err:
+            shared.LOG_ERROR(f'an error occurred while retrieving message header.\n\t{err}')
+            continue
 
-x = Thread(target=output_recvfrom, args=(sock, ))
-x.start()
+        # receive data bytes
+        try:
+            bytes_data = server_sock.recv(shared._HEADER_SIZE)
+            data       = bytes_data.decode()
+        except OSError as err:
+            shared.LOG_ERROR(f'an error occurred while retrieving message data.\n\t{err}')
+            continue
+        
+        # ignore any protocol messages that aren't direct messages
+        if _type != 3:
+            continue
 
+        # unpack message
+        sender, recipient, message = data.split('\0', 2)
+        print(f'{sender} -> {recipient}: {message}')
+
+# port selection
+port_index = shared.port_select(shared._PORTS)
+
+# handshake attempt with selected server
+server_sock = shared.attempt_handshake(shared._LOCALHOST, shared._PORTS[port_index])
+if server_sock is None:
+    shared.LOG_MESSAGE('Closing client...')
+    input('press any key to exit.')
+    exit()
+
+# send username to the server
+username = input('Enter your name: ')
+shared.set_username(server_sock, username)
+
+# create listener for the server
+server_listener = threading.Thread(target=await_messages, args=(server_sock,))
+server_listener.start()
+
+# get user input and send via server
 for line in sys.stdin:
-    sock.sendto(line.strip().encode(), server_addr)
+    recipient, message = line.strip().split(' ', 1)
+    shared.send_message(server_sock, username, recipient, message)
 
-sock.close()
-x.join()
+# close connection with server
+server_sock.close()
+server_listener.join()
 
+shared.LOG_MESSAGE('done.')
