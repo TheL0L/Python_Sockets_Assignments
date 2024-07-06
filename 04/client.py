8c2aa54e570ce086cec114fa9aacb5453889f3ea
Sergei <sergeiyakima1@gmail.com>
2024-07-06 23:53:56 +0300

    implemented client-side pinging

--- 04/client.py
+++ 04/client.py
@@ -1,4 +1,4 @@
-import socket, sys, threading
+import socket, sys, threading, time
 import shared
 
 def await_messages(server_sock: socket.socket):
@@ -24,11 +24,52 @@ def await_messages(server_sock: socket.socket):
         sender, recipient, message = response['data'].split('\0', 2)
         print(f'{sender} -> {recipient}: {message}')
 
-# port selection
-port_index = shared.port_select(shared._PORTS)
+def measure_rtt(server_sock: socket.socket) -> float:
+    if server_sock is None:
+        return -1
+    
+    # start measuring rtt
+    rtt = time.time()
 
-# handshake attempt with selected server
-server_sock = shared.attempt_handshake(shared._LOCALHOST, shared._PORTS[port_index])
+    # send ping and wait for a reply
+    shared.send_ping(server_sock)
+    response = shared.receive_via_socket(server_sock)
+
+    # end measuring rtt
+    rtt = time.time() - rtt
+
+    # check for any errors
+    if response['error'] is not None:
+        return -1
+    
+    # check for reply validity
+    if response['type'] != 4 or response['sub_type'] != 1:
+        return -1
+    
+    return rtt
+
+
+RTTs = {}
+
+# measure rtt for each server
+for port in shared._PORTS:
+    server_sock = shared.attempt_handshake(shared._LOCALHOST, port)
+    if server_sock is None:
+        RTTs[port] = -1
+        continue
+    RTTs[port] = measure_rtt(server_sock)
+    server_sock.close()
+
+# discard failed ports
+RTTs = {port: rtt for port, rtt in RTTs.items() if rtt >= 0}
+
+if len(RTTs) == 0:
+    shared.LOG_MESSAGE('Could not connect to any server, closing client...')
+    input('press any key to exit.')
+    exit()
+
+# connect to the server with the lowest rtt
+server_sock = shared.attempt_handshake(shared._LOCALHOST, min(RTTs, key=RTTs.get))
 if server_sock is None:
     shared.LOG_MESSAGE('Closing client...')
     input('press any key to exit.')
